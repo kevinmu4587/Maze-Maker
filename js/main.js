fa590d204b54ff10237bad234e1bb03f20767f2d
Kevin Moo <kevinmu385@gmail.com>
2021-06-29 21:03:52 -0400

    shorten collision code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 let app, player;
-let keys = {};
-let keysDiv;
 let walls = [];
 
 // constants for player movement
@@ -94,145 +92,64 @@ window.onload = function() {
     app.stage.addChild(player);
     render();
 
-    /*
-    for(i=0; i < 20; i++) {
-        wall = new PIXI.Sprite.from("images/brick.jpg");
-        wall.x = app.view.width / 2 + 32;
-        wall.y = app.view.height - (i+1) * 32;
-        wall.width = 32;
-        wall.height = 32;
-        app.stage.addChild(wall);
-        walls.push(wall);
-    }
-
-
-    for(i=0; i < 20; i++) {
-        wall = new PIXI.Sprite.from("images/brick.jpg");
-        wall.x = app.view.width / 2 - 64;
-        wall.y = app.view.height - (i+1) * 32;
-        wall.width = 32;
-        wall.height = 32;
-        app.stage.addChild(wall);
-        walls.push(wall);
-    }
-    */
-
-    // // set the finish
-    // finish = new PIXI.Sprite.from("images/finish.png");
-    // finish.x = PIXEL_WIDTH / 2;
-    // finish.y = 0;
-    // finish.width = TILE_WIDTH;
-    // finish.height = TILE_HEIGHT;
-    // app.stage.addChild(finish);
-    // fBox = finish.getBounds();
-
     // keyboard event listeners
-    window.addEventListener("keydown", keypress);
     window.addEventListener("keyup", keysUp);
 
     // ticker to call gameLoop function during Pixi eventhandler
     app.ticker.add(gameLoop);
-    // to display key info in the body
-    keysDiv = document.querySelector("#keys");
-
-    function keypress (e) {
-        //console.log(e.keyCode);
-        keys[e.keyCode] = true;
-    }
 
     function keysUp (e) {
         key = e.keyCode;
         if (key == LEFT || key == A) {
-            player.x -= collisionLeft();
+            player.x -= collision(player.x - TILE_WIDTH / 2 - 1, player.y);
             console.log(e.keyCode);
         } else if (key == RIGHT || key == D) {
-            player.x += collisionRight();
+            player.x += collision(player.x + TILE_WIDTH / 2 + 1, player.y);
         } else if (key == UP || key == W) {
-            player.y -= collisionUp();
+            player.y -= collision(player.x, player.y - TILE_WIDTH / 2 - 1);
         } else if (key == DOWN || key == S) {
-            player.y += collisionDown();
+            player.y += collision(player.x, player.y + TILE_WIDTH / 2);
         }
-        keys[e.keyCode] = false;
     }
 
     function gameLoop() {
-        keysDiv.innerHTML = JSON.stringify(keys);
-        pBox = player.getBounds();
-        // check if finished
-        /*
-        console.log("pBox.x", pBox.x);
-        console.log("pBox.y", pBox.y);
-        console.log("fBox.x", fBox.x);
-        console.log("fBox.y", fBox.y);
-        */
-        // if(pBox.x + pBox.width > fBox.x && pBox.y + pBox.height < fBox.y + fBox.height) {
-        //     console.log("FINSIH")
-        //     win = new PIXI.Sprite.from("images/winMsg.png");
-        //     win.x = 0;
-        //     win.y = 0;
-        //     app.stage.addChild(win);
-        // }
-        
-        // player movement
-        // if (keys[W] || keys[UP] && player.y > 0) { player.y -= collisionUp(); } 
-        // if (keys[A] || keys[LEFT] && player.x > 0) { player.x -= collisionLeft(); }
-        // if (keys[S] || keys[DOWN] && player.y + (TILE_WIDTH / 2) < PIXEL_HEIGHT) { player.y += collisionDown(); }
-        // if (keys[D] || keys[RIGHT] && player.x < PIXEL_WIDTH) { player.x += collisionRight(); }
     }
 
     // getTile(px, py): returns the value of the tile at pixel coordinates px and py
     function getTile(px, py) {
         tx = Math.floor(px / TILE_WIDTH);
-        ty = Math.floor(py / TILE_HEIGHT);
-        // console.log("tx:" + tx);
-        // console.log("ty:" + ty);    
+        ty = Math.floor(py / TILE_HEIGHT); 
         return [tx, ty];
     }
 
-    function collisionDown() {
-        tile = getTile(player.x, player.y + TILE_WIDTH / 2);
-        tx = tile[0];
-        ty = tile[1];
-        if (ty == NUM_TILES_Y || tiles[ty][tx] == 1) {
-            console.log("below tile is wall");
-            return 0;
-        }
-        return SPEED;
-    } 
-
-    function collisionUp() {
-        tile = getTile(player.x, player.y - TILE_WIDTH / 2 - 1);
-        tx = tile[0];
-        ty = tile[1];
-        if (ty == -1 || tiles[ty][tx] == 1) {
-            console.log("above tile is wall");
-            //player.y = (ty + 1) * TILE_WIDTH + 200;
-            return 0;
-        }
-        return SPEED;
+    function victory() {
+        win = new PIXI.Sprite.from("images/winMsg.png");
+        win.x = 0;
+        win.y = 0;
+        win.width = PIXEL_WIDTH;
+        win.height = PIXEL_HEIGHT;
+        app.stage.addChild(win);
     }
 
-    function collisionLeft() {
-        tile = getTile(player.x - TILE_WIDTH / 2 - 1, player.y);
-        tx = tile[0];
-        ty = tile[1];
-        if (tx == -1 || tiles[ty][tx] == 1) {
-            console.log("tile left of player is a wall")
-            //player.x = tx * (TILE_WIDTH + 1) + 32;
+    function collision(targetX, targetY) {
+        targetTile = getTile(targetX, targetY);
+        tx = targetTile[0];
+        ty = targetTile[1];
+        // console.log("tx: " + tx);
+        // console.log("ty: " + ty);
+        if (tx < 0 || tx >= NUM_TILES_X || ty < 0 || ty >= NUM_TILES_Y) {
+            console.log("trying to move outside the board");
             return 0;
-        }
-        return SPEED;
-    }
-
-    function collisionRight() {
-        tile = getTile(player.x + TILE_WIDTH / 2, player.y);
-        tx = tile[0];
-        ty = tile[1];
-        if (tx == NUM_TILES_X || tiles[ty][tx] == 1) {
-            console.log("right tile is wall");
+        } else if (tiles[ty][tx] == TILE_WALL) {
+            console.log("trying to move into a wall");
             return 0;
-            //pBox = player.getBounds();
+        } else if (tiles[ty][tx] == TILE_FINISH) {
+            victory();
+            console.log("victory!");
+            return SPEED;
+        } else {
+            console.log("moving...");
+            return SPEED;
         }
-        return SPEED;
     }
 }
